e54a03de42604394375fed776b594ac58f259631
Roberto Lillo <robertoxlp@hotmail.com>
2018-12-27 18:22:25 -0300

    Avance

--- pieza.java
+++ pieza.java
@@ -8,25 +8,11 @@ public class Pieza {
     //      11      1      11      11      111      1        1
     //              1      1        1               11      11
     //              1                          
-    private int[] pieza10 = {0, 0, 0, 1, 1, 0, 1, 1, 2, 2};
-    private int[] pieza20 = {0, 0, 1, 0, 2, 0, 3, 0, 1, 4};
-    private int[] pieza21 = {0, 0, 0, 1, 0, 2, 0, 3, 4, 1};
-    private int[] pieza30 = {0, 0, 1, 0, 1, 1, 2, 1, 2, 3};
-    private int[] pieza31 = {0, 1, 0, 2, 1, 0, 1, 1, 3, 2};
-    private int[] pieza40 = {0, 1, 1, 0, 1, 1, 2, 0, 2, 3};
-    private int[] pieza41 = {0, 0, 0, 1, 1, 1, 1, 2, 3, 2};
-    private int[] pieza50 = {0, 0, 0, 1, 0, 2, 1, 1, 3, 2};
-    private int[] pieza51 = {0, 0, 1, 0, 1, 1, 2, 0, 2, 3};
-    private int[] pieza52 = {0, 1, 1, 0, 1, 1, 1, 2, 3, 2};
-    private int[] pieza53 = {0, 1, 1, 0, 1, 1, 2, 1, 2, 3};
-    private int[] pieza60 = {0, 0, 0, 1, 1, 0, 2, 0, 2, 3};
-    private int[] pieza61 = {0, 0, 1, 0, 1, 1, 1, 2, 3, 2};
-    private int[] pieza62 = {0, 1, 1, 1, 2, 0, 2, 1, 2, 3};
-    private int[] pieza63 = {0, 0, 0, 1, 0, 2, 1, 2, 3, 2};
-    private int[] pieza70 = {0, 0, 0, 1, 1, 1, 2, 1, 2, 3};
-    private int[] pieza71 = {0, 0, 0, 1, 0, 2, 1, 0, 3, 2};
-    private int[] pieza72 = {0, 0, 1, 0, 2, 0, 2, 1, 2, 3};
-    private int[] pieza73 = {0, 2, 1, 0, 1, 1, 1, 2, 3, 2};
+
+    private int[] piezaR0;
+    private int[] piezaR1;
+    private int[] piezaR2;
+    private int[] piezaR3;
 
     private int idPieza, rotaciones;
     private int[] piezaActual;
@@ -37,25 +23,63 @@ public class Pieza {
         rotaciones = 0;
         switch(idPieza) {
             case 1:
-                piezaActual = pieza10;
+                int[] pieza10 = {0, 0, 0, 1, 1, 0, 1, 1, 2, 2};
+                this.piezaR0 = pieza10;
+                piezaActual = this.piezaR0;
                 break;
             case 2:
-                piezaActual = pieza20;
+                int[] pieza20 = {0, 0, 1, 0, 2, 0, 3, 0, 1, 4};
+                int[] pieza21 = {0, 0, 0, 1, 0, 2, 0, 3, 4, 1};
+                this.piezaR0 = pieza20;
+                this.piezaR1 = pieza21;
+                piezaActual = this.piezaR0;
                 break;
             case 3:
-                piezaActual = pieza30;
+                int[] pieza30 = {0, 0, 1, 0, 1, 1, 2, 1, 2, 3};
+                int[] pieza31 = {0, 1, 0, 2, 1, 0, 1, 1, 3, 2};
+                this.piezaR0 = pieza30;
+                this.piezaR1 = pieza31;
+                piezaActual = this.piezaR0;
                 break;
             case 4:
-                piezaActual = pieza40;
+                int[] pieza40 = {0, 1, 1, 0, 1, 1, 2, 0, 2, 3};
+                int[] pieza41 = {0, 0, 0, 1, 1, 1, 1, 2, 3, 2};
+                this.piezaR0 = pieza40;
+                this.piezaR1 = pieza41;
+                piezaActual = this.piezaR0;
                 break;
             case 5:
-                piezaActual = pieza50;
+                int[] pieza50 = {0, 0, 0, 1, 0, 2, 1, 1, 3, 2};
+                int[] pieza51 = {0, 0, 1, 0, 1, 1, 2, 0, 2, 3};
+                int[] pieza52 = {0, 1, 1, 0, 1, 1, 1, 2, 3, 2};
+                int[] pieza53 = {0, 1, 1, 0, 1, 1, 2, 1, 2, 3};
+                this.piezaR0 = pieza50;
+                this.piezaR1 = pieza51;
+                this.piezaR2 = pieza52;
+                this.piezaR3 = pieza53;
+                piezaActual = this.piezaR0;
                 break;
             case 6:
-                piezaActual = pieza60;
+                int[] pieza60 = {0, 0, 0, 1, 1, 0, 2, 0, 2, 3};
+                int[] pieza61 = {0, 0, 1, 0, 1, 1, 1, 2, 3, 2};
+                int[] pieza62 = {0, 1, 1, 1, 2, 0, 2, 1, 2, 3};
+                int[] pieza63 = {0, 0, 0, 1, 0, 2, 1, 2, 3, 2};
+                this.piezaR0 = pieza60;
+                this.piezaR1 = pieza61;
+                this.piezaR2 = pieza62;
+                this.piezaR3 = pieza63;
+                piezaActual = this.piezaR0;
                 break;
             case 7:
-                piezaActual = pieza70;
+                int[] pieza70 = {0, 0, 0, 1, 1, 1, 2, 1, 2, 3};
+                int[] pieza71 = {0, 0, 0, 1, 0, 2, 1, 0, 3, 2};
+                int[] pieza72 = {0, 0, 1, 0, 2, 0, 2, 1, 2, 3};
+                int[] pieza73 = {0, 2, 1, 0, 1, 1, 1, 2, 3, 2};
+                this.piezaR0 = pieza70;
+                this.piezaR1 = pieza71;
+                this.piezaR2 = pieza72;
+                this.piezaR3 = pieza73;
+                piezaActual = this.piezaR0;
                 break;
             // Sin default porque al constructor solo entran valores entre 1 y 7 para id.
         }
@@ -116,11 +140,11 @@ public class Pieza {
             case 2:
                 switch(rotaciones) {
                     case 0:
-                        piezaActual = pieza21;
+                        piezaActual = this.piezaR1;
                         rotaciones = 1;
                         break;
                     case 1:
-                        piezaActual = pieza20;
+                        piezaActual = this.piezaR0;
                         rotaciones = 0;
                         break;
                 }
@@ -129,11 +153,11 @@ public class Pieza {
             case 3:
                 switch(rotaciones) {
                     case 0:
-                        piezaActual = pieza31;
+                        piezaActual = this.piezaR1;
                         rotaciones = 1;
                         break;
                     case 1:
-                        piezaActual = pieza30;
+                        piezaActual = this.piezaR0;
                         rotaciones = 0;
                         break;
                 }
@@ -142,11 +166,11 @@ public class Pieza {
             case 4:
                 switch(rotaciones) {
                     case 0:
-                        piezaActual = pieza41;
+                        piezaActual = this.piezaR1;
                         rotaciones = 1;
                         break;
                     case 1:
-                        piezaActual = pieza40;
+                        piezaActual = this.piezaR0;
                         rotaciones = 0;
                         break;
                 }
@@ -155,19 +179,19 @@ public class Pieza {
             case 5:
                 switch(rotaciones) {
                     case 0:
-                        piezaActual = pieza51;
+                        piezaActual = this.piezaR1;
                         rotaciones = 1;
                         break;
                     case 1:
-                        piezaActual = pieza52;
+                        piezaActual = this.piezaR2;
                         rotaciones = 2;
                         break;
                     case 2:
-                        piezaActual = pieza53;
+                        piezaActual = this.piezaR3;
                         rotaciones = 3;
                         break;
                     case 3:
-                        piezaActual = pieza50;
+                        piezaActual = this.piezaR0;
                         rotaciones = 0;
                         break;
                 }
@@ -176,19 +200,19 @@ public class Pieza {
             case 6:
                 switch(rotaciones) {
                     case 0:
-                        piezaActual = pieza61;
+                        piezaActual = this.piezaR1;
                         rotaciones = 1;
                         break;
                     case 1:
-                        piezaActual = pieza62;
+                        piezaActual = this.piezaR2;
                         rotaciones = 2;
                         break;
                     case 2:
-                        piezaActual = pieza63;
+                        piezaActual = this.piezaR3;
                         rotaciones = 3;
                         break;
                     case 3:
-                        piezaActual = pieza60;
+                        piezaActual = this.piezaR0;
                         rotaciones = 0;
                         break;
                 }
@@ -197,19 +221,19 @@ public class Pieza {
             case 7:
                 switch(rotaciones) {
                     case 0:
-                        piezaActual = pieza71;
+                        piezaActual = this.piezaR1;
                         rotaciones = 1;
                         break;
                     case 1:
-                        piezaActual = pieza72;
+                        piezaActual = this.piezaR2;
                         rotaciones = 2;
                         break;
                     case 2:
-                        piezaActual = pieza73;
+                        piezaActual = this.piezaR3;
                         rotaciones = 3;
                         break;
                     case 3:
-                        piezaActual = pieza70;
+                        piezaActual = this.piezaR0;
                         rotaciones = 0;
                         break;
                 }
